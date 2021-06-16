141d8bc91339a42d2f578eb14ea6e2eebac2b20b
jjh <jujinhyen@gmail.com>
2021-06-16 15:53:29 +0900

    Chpater 14

---
+++ test1.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+
+</head>
+<body>
+<table border="1">
+    <tr>
+        <td>성명</td>
+        <td width="400"></td>
+        <td>학년</td>
+        <td width="400"></td>
+    </tr>
+    <tr>
+        <td height="200" width="1200"></td>
+    </tr>
+    <tr>
+        <td align="right">
+            <input type="submit" align="right" value="보내기">
+        </td>
+    </tr>
+</table>
+</body>
+</html>
